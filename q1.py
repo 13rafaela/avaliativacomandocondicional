619e2c838aa698cec245bb2a9d43a014ac5fbe49
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-15 14:15:58 -0300

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+num = int(input(' Digite um número de 0 á 999:'))
+x = 1 
+if num <= 10000:
+    soma = num+x
+    print('a soma dos algarismos é igual a:', soma)
+else: 
+    print('numero inválido')
